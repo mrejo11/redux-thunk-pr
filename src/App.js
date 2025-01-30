acdadd55554812c605c8f8f02c88d1ccabe707d1
mehdi <mreko95@protonmail.com>
2025-01-30 13:42:12 +0330

    RTKquery

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import UsersList from './components/UsersList';
-
 function App() {
   return (
     <div className="container mx-auto">
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
+import { albumsApi } from "./apis/albumsApi";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
   },
 });
-export * from './thunks/fetchUsers';
-export * from './thunks/addUser';
-export * from './thunks/removeUsers';
+
+setupListeners(store.dispatch)
+export * from "./thunks/fetchUsers";
+export * from "./thunks/addUser";
+export * from "./thunks/removeUsers";
+export {useFetchAlbumsQuery,useAddAlbumMutation} from './apis/albumsApi'
